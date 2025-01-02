0464b382afb8716c07ab558900b9206830d4413a
jjserantes <jserantes@gmail.com>
2025-01-02 19:50:19 +0100

    Continuando con el Main

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,23 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+    var imgUser = document.getElementById("imgUser");
+    var userMenu = document.getElementById("userMenu");
+
+    imgUser.addEventListener("click", function() {
+        if (userMenu.style.display === "block") {
+            userMenu.style.display = "none";
+        } else {
+            userMenu.style.display = "block";
+        }
+    });
+
+    // Opcional: Cerrar el menú cuando se hace clic en cualquier parte fuera del menú
+    document.addEventListener("click", function(event) {
+        if (!imgUser.contains(event.target) && !userMenu.contains(event.target)) {
+            userMenu.style.display = "none";
+        }
+    });
+});*/
+
 document.addEventListener("DOMContentLoaded", function() {
     var imgUser = document.getElementById("imgUser");
     var userMenu = document.getElementById("userMenu");
@@ -16,4 +36,18 @@ document.addEventListener("DOMContentLoaded", function() {
             userMenu.style.display = "none";
         }
     });
+
+    // Funcionalidad para el desplegable de 'asidemovil' en modo responsive
+    const asidemovil = document.querySelector('.asidemovil');
+    const ul = asidemovil.querySelector('ul');
+
+    asidemovil.addEventListener('click', function() {
+        if (ul.style.display === 'block') {
+            ul.style.display = 'none';
+        } else {
+            ul.style.display = 'block';
+        }
+    });
 });
+
+
